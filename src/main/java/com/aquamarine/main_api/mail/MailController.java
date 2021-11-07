cee424ffb3862f8bf7a227fe37e1b653c5519e4c
jungmincha <prttyjimmy@naver.com>
2021-11-07 17:07:11 +0900

    이메일 인증 서비스 구현

--- src/main/java/com/aquamarine/main_api/mail/MailController.java
+++ src/main/java/com/aquamarine/main_api/mail/MailController.java
@@ -1,7 +1,11 @@
 package com.aquamarine.main_api.mail;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,17 +16,17 @@ public class MailController {
     @Autowired
     private MailService mailService;
 
-    @GetMapping("/send")
-    public MailDto sendTestMail(String email){
-        MailDto mailDto = new MailDto();
+    @PostMapping("/send/{email}")
+    public Map<String, Object> sendTestMail(@PathVariable String email){
+        // MailDto mailDto = new MailDto();
         
-        mailDto.setAddress(email);
-        mailDto.setTitle("정민님이 발송한 이메일입니다.");
-        mailDto.setMessage("안녕하세여");
+        // mailDto.setAddress(email);
+        // mailDto.setTitle("정민님이 발송한 이메일입니다.");
+        // mailDto.setMessage("안녕하세여");
 
-        mailService.sendTestMail(mailDto);
+        //mailService.sendTestMail(mailDto);
 
-        return mailDto;
+        return mailService.sendTestMail(email);
     }
-    
+
 }
